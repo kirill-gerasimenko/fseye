f1c754eb0e489092718fd8ecb78fb4c30d45a81f
Kirill Gerasimenko <kirill.gerasimenko@outlook.com>
2016-10-10 09:33:12 +0200

    Make build script to package only linqpad plugin along with FsEye

--- build.fsx
+++ build.fsx
@@ -4,22 +4,29 @@
 open Fake
 
 // Directories
-let buildDir  = "./build/"
+let deployDir           = "./deploy/"
+let buildDir            = "./build/"
+let pluginsBuildDir     = buildDir </> "plugins"
+let pluginsDeployDir    = deployDir </> "plugins"
+let pluginsProjects     = ["./src/FsEye.LinqPad.Plugin/FsEye.LinqPad.Plugin.fsproj"]
 
 // Targets
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir]
+    CleanDirs [buildDir;deployDir]
 )
 
-Target "Build" (fun _ ->
-    // compile all projects below src/app/
-    MSBuildDebug buildDir "Build" appReferences
-    |> Log "AppBuild-Output: "
-)
+Target "Build" <| fun _ ->
+    MSBuildRelease buildDir "Build" ["./src/FsEye/FsEye.fsproj"] |> Log "AppBuild-Output: "
+    MSBuildRelease pluginsBuildDir "Build" pluginsProjects |> Log "AppBuild-Output: "
+
+Target "CopyArtefacts" <| fun _ ->
+    Copy deployDir ([ "FsEye.dll"; "FSharp.Core.dll"; "Utils.dll" ] |> Seq.map ((</>) buildDir))
+    Copy pluginsDeployDir ([ "FsEye.LinqPad.Plugin.dll"; "LINQPad.exe" ] |> Seq.map ((</>) pluginsBuildDir))
 
 // Build order
 "Clean"
   ==> "Build"
+  ==> "CopyArtefacts"
 
 // start build
-RunTargetOrDefault "Build"
+RunTargetOrDefault "CopyArtefacts"
